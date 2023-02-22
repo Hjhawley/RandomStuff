6b9ac848be441b208498a973b1df9a59105221b8
Hayden Hawley <hjhawley7@gmail.com>
2023-02-22 16:43:16 -0700

    albums thing

---
+++ 1001albums.py
@@ -0,0 +1,17 @@
+import random
+
+print("Can't decide what to listen to?")
+albumList = []
+try:
+    with open("albums.txt", "r", encoding="UTF-8") as file:
+        for line in file:
+            albumList.append(line.strip())
+    if albumList:
+        userInput = input("Press ENTER for a random album, Q to quit. ")
+        while userInput.upper() != "Q":
+            randomIndex = random.randint(0, len(albumList) - 1)
+            userInput = input(str(randomIndex + 1) + ". " + albumList[randomIndex] + " ")
+    else:
+        print("Error: File is empty.")
+except FileNotFoundError:
+    print("Error: File not found.")
