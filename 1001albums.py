9ea37777f0d9821c2a83ce3ce9c82a1cef22e0b2
Hayden Hawley <hjhawley7@gmail.com>
2023-03-03 00:21:40 -0700

    Mk 2

--- 1001albums.py
+++ 1001albums.py
@@ -1,31 +1,76 @@
 import random
+import re
+ALBUMS_FILE = "albums.txt"
 
 def main():
     print("Can't decide what to listen to?")
+    print("Hit ENTER for a random album.")
+    print("Type Menu to see more options.")
+    handle_input(load_album_dict())
+
+def load_album_dict():
     albumDict = {}
     try:
-        with open("albums.txt", "r", encoding="UTF-8") as file:
+        with open(ALBUMS_FILE, "r", encoding="UTF-8") as file:
             albumNumber = 1
             for line in file:
                 line = line.strip()
                 if line: # Ignore empty lines
-                    artist, albumTitleYear = line.split(" – ")
-                    albumTitle, year = albumTitleYear.rsplit("(", 1)
-                    year = int(year.strip(")"))
-                    albumDict[albumNumber] = {"artist": artist, "title": albumTitle.strip(), "year": year}
-                    albumNumber += 1
-
-            if albumDict:
-                userInput = input("Press ENTER for a random album, Q to quit. ")
-                while userInput.upper() != "Q":
-                    randomIndex = random.randint(1, len(albumDict))
-                    album = albumDict[randomIndex]
-                    print(f"{randomIndex}. {album['artist']} - {album['title']} ({album['year']})")
-                    userInput = input()
-            else:
+                    match = re.match(r"^(.+) – (.+) \((\d+)\)$", line)
+                    if match:
+                        artist, albumTitle, year = match.groups()
+                        year = int(year)
+                        albumDict[albumNumber] = {"artist": artist, "title": albumTitle, "year": year}
+                        albumNumber += 1
+            if not albumDict:
                 print("Error: File is empty.")
     except FileNotFoundError:
         print("Error: File not found.")
+    return albumDict
+
+def user_menu():
+    print("Hit ENTER for a random album.")
+    print("Type Next to see the next album in the list.")
+    print("Type Last to see the previous album in the list.")
+    print("Type a year to limit albums to that year.")
+    print("Type Q to quit.")
+
+def handle_input(albumDict):
+    while True:
+        userInput = input("Command? ")
+        if not userInput: # empty input
+            random_album(albumDict)
+        elif userInput.isdigit():
+            random_album_by_year(albumDict, int(userInput))
+        elif userInput.upper() == "Q":
+            break
+        elif userInput.upper() == "MENU":
+            user_menu()
+        else:
+            print("Invalid input. Type Menu to see options.")
+
+def print_albums(albums, message):
+    if not albums:
+        print(f"No albums found.")
+        return
+    for i, album in enumerate(albums, start=1):
+        print(f"{i}. {album['artist']} - {album['title']} ({album['year']})")
+    if message:
+        print(message)
+
+def random_album(albumDict):
+    randomIndex = random.randint(1, len(albumDict))
+    album = albumDict[randomIndex]
+    print(f"{randomIndex}. {album['artist']} - {album['title']} ({album['year']})")
+
+def random_album_by_year(albumDict, year):
+    year_albums = [album for album in albumDict.values() if album['year'] == year]
+    if not year_albums:
+        print(f"No albums found for the year {year}.")
+        return
+    album = random.choice(year_albums)
+    album_number = list(albumDict.keys())[list(albumDict.values()).index(album)]
+    print(f"{album_number}. {album['artist']} - {album['title']} ({album['year']})")
 
 if __name__ == "__main__":
     main()
