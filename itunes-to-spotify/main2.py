547b71a11d4597071920dbd61eb69fa3952575d8
Hayden Hawley <hjhawley7@gmail.com>
2023-04-13 11:36:01 -0600

    v2 - Significantly improved search results

--- itunes-to-spotify/main2.py
+++ itunes-to-spotify/main2.py
@@ -6,9 +6,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 def clean_track_title(title):
-    # Remove or replace special characters
-    cleaned_title = re.sub(r'[\/\-]', '', title)
-    # Remove text within parentheses
+    cleaned_title = re.sub(r'[\/\-]', ' ', title)  # Replace with spaces
     cleaned_title = re.sub(r'\(.*\)', '', cleaned_title).strip()
     return cleaned_title
 
@@ -19,56 +17,48 @@ def find_best_track_match(tracks, query):
         track_name = track['name']
         artist_name = track['artists'][0]['name']
         popularity = track['popularity']
-        # Calculate the fuzzy string match score for the track
         match_score = fuzz.partial_ratio(query.lower(), f"{artist_name} {track_name}".lower())
-        # Prioritize tracks with higher match scores and popularity
         if match_score > best_match_score or (match_score == best_match_score and popularity > best_match['popularity']):
             best_match = track
             best_match_score = match_score
     return best_match
 
-def main():
-    # Prompt user for name of iTunes playlist XML file
-    xml_file = input("Name of the iTunes playlist XML file: ")
+def process_track(track):
+    name, artist, album = None, None, None
+    children = list(track)
+    for i, child in enumerate(children):
+        if child.tag == "key":
+            if child.text == "Name" and i + 1 < len(children) and children[i + 1].tag == "string":
+                name = children[i + 1].text
+            elif child.text == "Artist" and i + 1 < len(children) and children[i + 1].tag == "string":
+                artist = children[i + 1].text
+            elif child.text == "Album" and i + 1 < len(children) and children[i + 1].tag == "string":
+                album = children[i + 1].text
+    return name, artist, album
 
-    # Load iTunes playlist XML
+def main():
+    xml_file = input("Name of the iTunes playlist XML file: ")
     tree = et.parse(xml_file)
     root = tree.getroot()
 
-    # Authenticate with Spotify API
     scope = 'playlist-modify-public'
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='d2b73b9ffc8b40c79aef4890db82237a', client_secret='818c0389ed9648f289235ef7e0fcc946', redirect_uri='http://localhost:8888/callback', scope=scope))
 
-    # Create a new Spotify playlist with the same name as the XML file
     playlist_name = os.path.splitext(xml_file)[0]
     user_id = sp.current_user()['id']
     playlist_id = sp.user_playlist_create(user_id, playlist_name)['id']
 
-    # Iterate through iTunes playlist and add tracks to Spotify playlist
     for track in root.findall("./dict/dict/dict"):
-        name = None
-        artist = None
-        album = None
-        children = list(track)
-        for i, child in enumerate(children):
-            if child.tag == "key":
-                if child.text == "Name" and i + 1 < len(children) and children[i + 1].tag == "string":
-                    name = children[i + 1].text
-                elif child.text == "Artist" and i + 1 < len(children) and children[i + 1].tag == "string":
-                    artist = children[i + 1].text
-                elif child.text == "Album" and i + 1 < len(children) and children[i + 1].tag == "string":
-                    album = children[i + 1].text
+        name, artist, album = process_track(track)
         if name and artist:
-            # Clean the track title
             cleaned_name = clean_track_title(name)
-            # Search for the track on Spotify
-            results = sp.search(q=f"track:{cleaned_name} artist:{artist} album:{album}", type='track')
+            cleaned_album = clean_track_title(album)
+            results = sp.search(q=f"track:{cleaned_name} artist:{artist} album:{cleaned_album}", type='track')
             tracks = results['tracks']['items']
-            if not tracks: # Search without the album information
+            if not tracks:
                 results = sp.search(q=f"track:{cleaned_name} artist:{artist}", type='track')
                 tracks = results['tracks']['items']
             if tracks:
-                # Find the best matching track
                 best_track = find_best_track_match(tracks, f"{artist} {cleaned_name}")
                 if best_track:
                     track_uri = best_track['uri']
@@ -76,7 +66,6 @@ def main():
                     print("Added", artist, "-", name, "to", xml_file)
                 else:
                     print("***", artist, "-", name, "could not be found. ***")
-            else:
-                print("***", artist, "-", name, "could not be found. ***")
 
-main()
+if __name__ == "__main__":
+    main()
